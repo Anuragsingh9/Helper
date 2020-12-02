e4f595157e2af73ebf9aa605b125c99c9e37b476
Anurag Singh <anuragsingh9999999@gmail.com>
2020-12-02 20:35:17 +0530

    Hleper

---
+++ Helper.php
@@ -0,0 +1,92 @@
+https://github.com/Anuragsingh9/Modules
+https://github.com/Anuragsingh9/NewProject
+C:\xampp\htdocs\work\newProject
+C:\xampp\htdocs\work\advops
+
+1. edited response for the schedule time(return as array)
+2. Read and implemented  all the Advance Oops topics
+2. created custome validation exception
+2.created api for task create and update
+3.Created documentation in postman also
+
+
+
+ *   required={"name","email","address"},
+     *   type="object",
+     * @SWG\Property(
+     *     format="int64",
+     *     description="ID",
+     *     title="ID",
+     *      )
+
+tomorrow we have to create swagger documentation for open api in laravel
+Hie this is anurag singh from gorakhpur upttar pradesh. 
+I am a Associate software engineer in pebibits technology
+
+
+
+
+
+
+
+
+//        $path = config('newsletter.s3.news_image');
+//        $fileName = time() . '.' . $blob->getClientOriginalExtension();
+//        return Storage::disk('s3')
+//            ->putFileAs($path, $blob, $fileName, 'public'); // to put the file on specific path with custom name,
+
+Notes->
+{}.Interfaces->Interfaces are the services provided by the laravel
+
+
+
+								To do list
+
+Features:-
+1.User can create task 
+2.Created task are only accessible to created user
+3.Can see the list of all task created by him
+4.Task would be of two types-completed and pending
+5.Can update task
+5.Can delete a task						
+  
+Api:-
+1.Task create
+2.List of all task
+3.List of task according to task status
+4.Changing status of task
+5.Update task
+6.Delete a task
+7.See a particular task
+
+
+Respected mam, 
+
+I want to say that my elder brother is getting married. There is no one else in the family to look after the arrangments. Therefore, I want  leave on 1st,9th,10th of December. Also may be there some emergency work,in that case please grant me leave if that situation comes.
+
+Thank you. 
+
+
+
+Coupling: coupling means how one module or object depends upon another object.
+Cohesion: cohesion means how elements are grouped together in a module.
+ Coupling should be always be loose coupling for better performence and cohesion should be high.
+
+
+ First the model should be filled with fillables in model file.
+ Then controller should be made and inside that we need to define the function in which all the logics should be there.
+ We add the parameters and the accordingly response is shown.
+ Then we have resource file in which we add the property which we need to show in the response.
+ and this resource file will return the response which will be taken by controller function.
+
+ added the functionality to where we can search the task according to the schedule_time of the task
+
+ Hie this anurag singh from Gorakhpur UP India
+ So this is a page where you can ask me to create a api on laravel
+
+
+Aggregation-- one or more  class is dependent on one main class
+
+Composition -- One class is directly dependent on each other. if one is gone other also gone.
+
+Association -- two classes are not dependent on each other. but they come together to perform a task.
